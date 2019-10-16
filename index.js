95bd09ed957b9790a211beddb9140afd47ab0537
Brandon-Harris <brandonharris177@gmail.com>
2019-10-16 14:22:35 -0600

    file outline complete

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const server = require('./server.js');
+const defaults = require('./config/default')
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+server.listen(defaults.port, () => {
+    console.log(`\n*** Server Running on http://localhost${defaults.port} ***\n`);
+  });
